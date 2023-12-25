e65fed4a47621fcb2ab9dd0e4722775332ff5fd3
Mahantesh Hiremath <mahanteshimath@gmail.com>
2023-12-26 02:23:32 +0530

    Update Leet570.py

--- pages/Leet570.py
+++ pages/Leet570.py
@@ -81,8 +81,9 @@ with right_column:
      if st.button("Execute Queries"):
           if query:
                with st.spinner("Executing all queries..."):
-                    # connection = create_snowflake_connection(account, role, warehouse, database, schema, user, password)
-                    result = execute_query(query)
+                    connection = create_snowflake_connection(account, role, warehouse, database, schema, user, password)
+                    cursor = connection.cursor()
+                    result =  cursor.execute(query)
                     st.success("Query executed!")
                     result_df = pd.DataFrame(result)
                     st.write(result_df)            

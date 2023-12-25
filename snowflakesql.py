5396debea82feb592e29135439209e722a4cb510
Mahantesh Hiremath <mahanteshimath@gmail.com>
2023-12-26 04:26:43 +0530

    Update snowflakesql.py

--- snowflakesql.py
+++ snowflakesql.py
@@ -8,6 +8,13 @@ import pandas as pd
 from st_pages import show_pages_from_config
 
 show_pages_from_config()
+
+st.code(Path(".streamlit/pages.toml").read_text(), language="toml")
+
+with st.echo("below"):
+    from st_pages import show_pages_from_config
+
+show_pages_from_config()
 st.set_page_config(
   page_title="Leetcodeforsnowflake",
   page_icon="  ",

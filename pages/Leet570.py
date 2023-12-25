7fadc58f0f98347f316b446ca034121e8eceb22f
Mahantesh Hiremath <mahanteshimath@gmail.com>
2023-12-26 03:33:25 +0530

    Update Leet570.py

--- pages/Leet570.py
+++ pages/Leet570.py
@@ -80,21 +80,23 @@ def main():
                 | John |
                 """)
         query = """
-        CREATE or replace TABLE Employee (
-            id INT,
-            name VARCHAR(50),
-            department VARCHAR(50),
-            managerId INT
-        ) AS (
-            SELECT * FROM VALUES
-                (101, 'John', 'A', NULL),
-                (102, 'Dan', 'A', 101),
-                (103, 'James', 'A', 101),
-                (104, 'Amy', 'A', 101),
-                (105, 'Anne', 'A', 101),
-                (106, 'Ron', 'B', 101)
-        );
-        """
+                    CREATE or replace TABLE Employee (
+                        id INT,
+                        name VARCHAR(50),
+                        department VARCHAR(50),
+                        managerId INT
+                    ) AS (
+                        SELECT * FROM VALUES
+                            (101, 'John', 'A', NULL),
+                            (102, 'Dan', 'A', 101),
+                            (103, 'James', 'A', 101),
+                            (104, 'Amy', 'A', 101),
+                            (105, 'Anne', 'A', 101),
+                            (106, 'Ron', 'B', 101)
+                    );
+                """
+         st.code(query, language='sql')
+
 
          if st.button("Prepare data"):
               if query:

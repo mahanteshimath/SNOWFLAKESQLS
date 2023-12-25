1ed0d282885ba7307935a9dab29439d9f0891129
Mahantesh Hiremath <mahanteshimath@gmail.com>
2023-12-26 04:56:25 +0530

    Update snowflakesql.py

--- snowflakesql.py
+++ snowflakesql.py
@@ -7,9 +7,9 @@ from st_pages import show_pages_from_config, add_page_title
 
 # Either this or add_indentation() MUST be called on each page in your
 # app to add indendation in the sidebar
-add_page_title()
-
-show_pages_from_config()
+    add_page_title()
+    
+    show_pages_from_config()
 
 st.set_page_config(
   page_title="Leetcodeforsnowflake",

8716417c0bf79ed6075e3a68c6a2982280175e50
Mahantesh Hiremath <mahanteshimath@gmail.com>
2023-12-26 02:38:10 +0530

    Update Leet570.py

--- pages/Leet570.py
+++ pages/Leet570.py
@@ -36,57 +36,57 @@ st.title('LeetCode 570. Managers with at Least 5 Direct Reports')
 left_column, right_column = st.columns(2)
 
 def main():
-with left_column:
-     st.markdown(""" 
-        ### Table: Employee
-
-            | Column Name | Type    |
-            |-------------|---------|
-            | id          | int     |
-            | name        | varchar |
-            | department  | varchar |
-            | managerId   | int     |
-
-            id is the primary key (column with unique values) for this table.
-            Each row of this table indicates the name of an employee, their department, and the id of their manager.
-            If managerId is null, then the employee does not have a manager.
-            No employee will be the manager of themselves.
-
-            Write a solution to find managers with at least five direct reports.
-
-            Return the result table in any order.
-
-            The result format is in the following example.
-
-            **Example 1:**
-
-            **Input:** 
-            Employee table:
-            | id  | name  | department | managerId |
-            |-----|-------|------------|-----------|
-            | 101 | John  | A          | null      |
-            | 102 | Dan   | A          | 101       |
-            | 103 | James | A          | 101       |
-            | 104 | Amy   | A          | 101       |
-            | 105 | Anne  | A          | 101       |
-            | 106 | Ron   | B          | 101       |
-
-            **Output:** 
-            | name |
-            |------|
-            | John |
-            """)
-
-with right_column:
-     query = st.text_area("Write query",height=250)
-     if st.button("Execute Queries"):
-          if query:
-               with st.spinner("Executing all queries..."):
-                    result = execute_query(query)
-                    st.success("Query executed!")
-                    result_df = pd.DataFrame(result)
-                    st.write(result_df)
-                    
+    with left_column:
+         st.markdown(""" 
+            ### Table: Employee
+    
+                | Column Name | Type    |
+                |-------------|---------|
+                | id          | int     |
+                | name        | varchar |
+                | department  | varchar |
+                | managerId   | int     |
+    
+                id is the primary key (column with unique values) for this table.
+                Each row of this table indicates the name of an employee, their department, and the id of their manager.
+                If managerId is null, then the employee does not have a manager.
+                No employee will be the manager of themselves.
+    
+                Write a solution to find managers with at least five direct reports.
+    
+                Return the result table in any order.
+    
+                The result format is in the following example.
+    
+                **Example 1:**
+    
+                **Input:** 
+                Employee table:
+                | id  | name  | department | managerId |
+                |-----|-------|------------|-----------|
+                | 101 | John  | A          | null      |
+                | 102 | Dan   | A          | 101       |
+                | 103 | James | A          | 101       |
+                | 104 | Amy   | A          | 101       |
+                | 105 | Anne  | A          | 101       |
+                | 106 | Ron   | B          | 101       |
+    
+                **Output:** 
+                | name |
+                |------|
+                | John |
+                """)
+    
+    with right_column:
+         query = st.text_area("Write query",height=250)
+         if st.button("Execute Queries"):
+              if query:
+                   with st.spinner("Executing all queries..."):
+                        result = execute_query(query)
+                        st.success("Query executed!")
+                        result_df = pd.DataFrame(result)
+                        st.write(result_df)
+                        
                     
 if __name__ == "__main__":
     main()
